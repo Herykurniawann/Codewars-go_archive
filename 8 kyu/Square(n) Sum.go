0199dfc0aaf29a6a849f529f30525a7689ea3015
Herykurniawann <herykurniawansartono@gmail.com>
2023-02-17 23:21:26 +0700

    commit

--- 8 kyu/Square(n) Sum.go
+++ 8 kyu/Square(n) Sum.go
@@ -1,10 +1,21 @@
 //https://www.codewars.com/kata/515e271a311df0350d00000f/train/go
 package main
 
+import "fmt"
+
 func main() {
 	//fmt.Println(math.Pow(2, 2))
+	var balance = []int{1, 2, 2}
+	fmt.Println(SquareSum(balance))
 }
 
-//func SquareSum(numbers []int) int {
-//	// your code here
-//}
+func SquareSum(numbers []int) int {
+	// your code here
+
+	var totalSum int
+	for i := 0; i < len(numbers); i++ {
+		totalSum = totalSum + (numbers[i] * numbers[i])
+
+	}
+	return totalSum
+}
