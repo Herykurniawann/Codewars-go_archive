b11a506c75d8ca421d08c3e42ace3831c9ca13b9
Herykurniawann <herykurniawansartono@gmail.com>
2023-02-17 23:06:11 +0700

    first commit

---
+++ Square(n) Sum.go
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/515e271a311df0350d00000f/train/go
+package main
+
+func main() {
+	//fmt.Println(math.Pow(2, 2))
+}
+
+//func SquareSum(numbers []int) int {
+//	// your code here
+//}
